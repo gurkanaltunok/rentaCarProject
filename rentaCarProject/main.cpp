b6ab5814c0f07730c79be63fab0199fd32cc1935
gurkan <oguzgurkan2001@gmail.com>
2024-12-14 14:35:30 +0200

    project updated

--- rentaCarProject/main.cpp
+++ rentaCarProject/main.cpp
@@ -12,50 +12,62 @@ void welcomeFunction(){
     cout<<"*** Welcome to Altunok Rent a Car ***"<<endl;
 }
 
-struct account{
-    char name[30];
-    char password[30];
-};
-
-struct car{
-    char brand[20];
-    char model[20];
+class Car {
+protected:
+    string brand;
+    string model;
     string id;
     int price;
+
+public:
+    Car(const string& brand, const string& model, const string& id, int price)
+        : brand(brand), model(model), id(id), price(price) {}
+
+    virtual void display() const {
+        cout << "Brand: " << brand << ", Model: " << model << ", ID: " << id << ", Price: " << price << endl;
+    }
+
+    virtual ~Car() {}
 };
 
-struct car cars[10]{
-    {"Mercedes","C220", "01", 2000},
-    {"Mercedes", "E350", "02", 2000},
-    {"Mazda", "Demio", "02", 1000},
-    {"Mazda", "Demio", "02", 1000},
-    {"Mazda", "Demio", "02", 1000},
-    {"Mazda", "Demio", "02", 1000},
-    {"Mazda", "Demio", "02", 1000},
-    {"Mazda", "Demio", "02", 1000},
-    {"Mazda", "Demio", "02", 1000},
-    {"Mazda", "Demio", "02", 1000},
+class PremiumCar : public Car {
+public:
+    PremiumCar(const string& brand, const string& model, const string& id, int price)
+        : Car(brand, model, id, price) {}
 
+    void display() const override {
+        cout << "[Premium] " << "Brand: " << brand << ", Model: " << model << ", ID: " << id << ", Price: " << price << endl;
+    }
 };
 
-void accountSelection(){
-    int selection;
+class EconomyCar : public Car {
+public:
+    EconomyCar(const string& brand, const string& model, const string& id, int price)
+        : Car(brand, model, id, price) {}
 
-
-}
+    void display() const override {
+        cout << "[Economy] " << "Brand: " << brand << ", Model: " << model << ", ID: " << id << ", Price: " << price << endl;
+    }
+};
 
 int main() {
-    int balance;
-    char askName[30];
-    char askEmail[30];
-    char askPhone[30];
-    char askPassword[30];
-    int selection;
-    int carSelection;
-    int number;
-    int i;
-
     welcomeFunction();
-    
+
+    vector<Car*> cars = {
+        new PremiumCar("Mercedes", "C220", "01", 2000),
+        new PremiumCar("Mercedes", "E350", "02", 2000),
+        new PremiumCar("BMW", "4.20d", "06", 1800),
+        new PremiumCar("Audi", "A5", "07", 1800),
+        new PremiumCar("Audi", "A3", "08", 1600),
+        new EconomyCar("Mazda", "Demio", "03", 1200),
+        new EconomyCar("Hyundai", "i20", "04", 600),
+        new EconomyCar("Suzuki", "Swift", "05", 800),
+        new EconomyCar("Volkswagen", "Golf", "09", 1200),
+        new EconomyCar("Nissan", "March", "10", 600)
+        };
+
+    for (const auto& car : cars) {
+        car->display();
+    }
     return 0;
 }
